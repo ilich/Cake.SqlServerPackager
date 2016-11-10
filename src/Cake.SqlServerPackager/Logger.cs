5ff42032aea5762fa10a4145c85036d4fbc5eeba
Ilya <iverbitskiy@gmail.com>
2016-11-10 22:49:49 +0100

    Added documentation.

--- src/Cake.SqlServerPackager/Logger.cs
+++ src/Cake.SqlServerPackager/Logger.cs
@@ -3,10 +3,20 @@ using Cake.Core.Diagnostics;
 
 namespace Cake.SqlServerPackager
 {
+    /// <summary>
+    /// Add-in logger.
+    /// </summary>
     public static class Logger
     {
+        /// <summary>
+        /// Gets or sets logger engine.
+        /// </summary>
         public static ICakeLog LogEngine { get; set; }
 
+        /// <summary>
+        /// Log a message.
+        /// </summary>
+        /// <param name="message">Message.</param>
         public static void Log(string message)
         {
             var text = $"Cake.SqlServerPackager: {message}";
--- src/Cake.SqlServerPackager/SqlServerPackagerExtensions.cs
+++ src/Cake.SqlServerPackager/SqlServerPackagerExtensions.cs
@@ -4,9 +4,17 @@ using Cake.Core.Annotations;
 
 namespace Cake.SqlServerPackager
 {
+    /// <summary>
+    /// Cake AddIn that packages multiple SQL Scripts into one file to speed up deployment and minimize the probability of errors.
+    /// </summary>
     [CakeAliasCategory("Database")]
     public static class SqlServerPackagerExtensions
     {
+        /// <summary>
+        /// Merge multiple SQL scripts into one SQL script.
+        /// </summary>
+        /// <param name="context">Cake context.</param>
+        /// <param name="settings">Settings.</param>
         [CakeMethodAlias]
         public static void SqlServerPackager(this ICakeContext context, SqlServerPackagerSettings settings)
         {
